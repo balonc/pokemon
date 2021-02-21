2486ad3f48d367d2692c105eaa1e0837917ff00d
Francisco Javier Balón Aguilar <fcojavierba@protonmail.com>
2021-02-21 14:21:01 +0100

    Avances iniciales

--- pokemon.c
+++ pokemon.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include "pokemon.h"
 
 int main(int argc, char **argv)
 {
     printf("Hola mundo pokemon\n");
+
+    for (int i = 0; i < 150; i++)
+    {
+        printf("%s\n", pokemons[i].nombre);
+    }
 
     return 0;
 }
