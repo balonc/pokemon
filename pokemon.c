df1ed1477248690f8a95de5fb35d5e48e73e0e88
Francisco Javier Balón Aguilar <fcojavierba@protonmail.com>
2021-03-08 20:14:41 +0100

    Mejorado sistema de argumentos, añadidos argumentos posicionales

--- pokemon.c
+++ pokemon.c
@@ -8,58 +8,31 @@
 int main(int argc, char **argv)
 {
 
-	int aflag = 0;
-	int bflag = 0;
-	char *cvalue = NULL;
-	int index;
 	int c;
+	char *options = "-ab:c:d::";
+	
+	c = getopt(argc, argv, options);
+	
+	while(c != -1) 
+	{
+		switch(c) {
 
-	opterr = 0;
+		case 'a'  :
+			printf("has seleccionado a\n");
+			break;
+			
+		case 'b'  :
+			printf("has seleccionado b con %s\n", optarg);
+			break;
+		}
 
-	while ((c = getopt (argc, argv, "abc:")) != -1)
-		switch (c)
+		if (strcmp(optarg,"abc") == 0)
 		{
-			case 'a':
-				aflag = 1;
-				break;
-			case 'b':
-				bflag = 1;
-				break;
-			case 'c':
-				cvalue = optarg;
-				break;
-			case '?':
-				if (optopt == 'c')
-					fprintf (stderr, "Option -%c requires an argument.\n", optopt);
-				else if (isprint (optopt))
-					fprintf (stderr, "Unknown option `-%c'.\n", optopt);
-				else
-					fprintf (stderr,
-					"Unknown option character `\\x%x'.\n",
-					optopt);
-				return 1;
-			default:
-		abort ();
+			printf("UEEE\n");
+		}
+		
+		c = getopt(argc, argv, options);
 	}
-
-	printf ("aflag = %d, bflag = %d, cvalue = %s\n",
-		aflag, bflag, cvalue);
-
-	for (index = optind; index < argc; index++)
-		printf ("Non-option argument %s\n", argv[index]);
-
-    for (int i = 0; i < 9; i++)
-    {
-        if (pokemons[i].id)
-        printf("%d - %s - %d %d\n", 
-            pokemons[i].id, pokemons[i].nombre, 
-            calculaPS(pokemons[i].baseps,0,0,100), 
-            calculaStat(pokemons[i].baseataque,0,0,100,1.1)
-        );
-    }
-
-	int b = calculaPrecision(100.0,0,0);
-	printf("%d", b);    
-
+	
     return 0;
 }
