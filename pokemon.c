bef2e9853de2ceee1472a96b0b190edef591ab3c
Francisco Javier Balón Aguilar <fcojavierba@protonmail.com>
2021-03-09 22:59:23 +0100

    Solución de errores con getopt

--- pokemon.c
+++ pokemon.c
@@ -9,31 +9,39 @@ int main(int argc, char **argv)
 {
 
 	int c;
-	char *options = "-ab:c:d::";
+	char *options = "-avzb:";
 	
+	int selectedV = 0;
+	int selectedZ = 0;
+
 	// optind, opterr, optopt, optopt, optarg
-	c = getopt(argc, argv, options);
+	//c = getopt(argc, argv, options);
 	
-	while(c != -1) 
+	while ((c = getopt (argc, argv, options)) != -1)
+	//while(c != -1) 
 	{
-		switch(c) {
-
-		case 'a'  :
-			printf("has seleccionado a\n");
-			break;
-			
-		case 'b'  :
-			printf("has seleccionado b con %s\n", optarg);
-			break;
-		}
-
-		if (strcmp(optarg,"abc") == 0)
+		switch(c) 
 		{
-			printf("UEEE\n");
+            case 'v':
+                selectedV = 1;
+                break;
+            case 'z':
+                selectedZ = 1;
+                break;
+            case 'b':
+                printf("selected b with %s\n", optarg);
+                break;
 		}
 
-		c = getopt(argc, argv, options);
+		if (optarg && (strcmp(optarg,"abc") == 0))
+		{
+			printf("ok\n");
+		}
+
+		//c = getopt(argc, argv, options);
 	}
+
+	printf("%d - %d\n", selectedV, selectedZ);
 	
     return 0;
 }
